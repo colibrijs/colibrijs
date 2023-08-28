2f972a1d74baf214bdeab0a1b9ca7da1eaf0cd86
Ivan Podgornov <ivan.podgornov@live.com>
2023-08-29 00:12:31 +0300

    ci: setup eslint

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,17 +1,12 @@
+import { address } from 'ip';
 import http from 'node:http';
-import ip from 'ip';
-import pg from 'pg';
 
-const client = new pg.Client({
-  user: process.env.DATABASE_USER ?? 'user',
-  password: process.env.DATABASE_PASSWORD ?? 'password',
-  host: process.env.DATABASE_HOST ?? '127.0.0.1',
-  port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
-  database: process.env.DATABASE_NAME ?? 'colibrijs',
-});
+import { client } from './pg-client.ts';
 
-client.connect()
-  .then(() => http
-    .createServer((_, res) => res.end(`My ip address is ${ip.address()}`, 'utf-8'))
-    .listen(3000, () => console.log('Server is running on the port 3000'))
+client
+  .connect()
+  .then(() =>
+    http
+      .createServer((_, res) => res.end(`My ip address is ${address()}`, 'utf-8'))
+      .listen(3000, () => console.log('Server is running on the port 3000'))
   );
