98fdcfc4684efb02cbbcb0e22b3e2cbdc8026077
Ivan Podgornov <ivan.podgornov@live.com>
2023-08-28 23:03:21 +0300

    ci: setup typescript

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -6,7 +6,7 @@ const client = new pg.Client({
   user: process.env.DATABASE_USER ?? 'user',
   password: process.env.DATABASE_PASSWORD ?? 'password',
   host: process.env.DATABASE_HOST ?? '127.0.0.1',
-  port: process.env.DATABASE_PORT ?? 5432,
+  port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
   database: process.env.DATABASE_NAME ?? 'colibrijs',
 });
 
