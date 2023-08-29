63ca2e06bf83b9bb41622ad81e78c5b012f246e6
Ivan Podgornov <ivan.podgornov@live.com>
2023-08-29 22:08:27 +0300

    ci: setup jest

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment -- verbatimModuleSyntax and config in ts are incompatible
+// @ts-nocheck
+
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  reporters: ['default', ['github-actions', { silent: false }]],
+  projects: ['api'].map((packageName) => ({
+    displayName: `@colibrijs/${packageName}`,
+    testMatch: [`<rootDir>/packages/${packageName}/src/**/*.spec.ts`],
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+    },
+  })),
+};
+
+export default jestConfig;
---
+++ packages/api/src/__tests__/sum.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from '@jest/globals';
+
+import { sum } from '../sum.ts';
+
+describe(sum.name, () => {
+  it('returns sum of two numbers', () => {
+    expect.assertions(1);
+    expect(sum(2, 3)).toBe(5);
+  });
+});
