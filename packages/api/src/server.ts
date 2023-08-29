cdec9c717aa3c524c7ee46353b6860cebd58f240
Ivan Podgornov <ivan.podgornov@live.com>
2023-08-29 23:54:09 +0300

    fix: disable import/no-named-as-default-member rule

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,4 +1,4 @@
-import { address } from 'ip';
+import ip from 'ip';
 import http from 'node:http';
 
 import { client } from './pg-client.ts';
@@ -7,6 +7,6 @@ client
   .connect()
   .then(() =>
     http
-      .createServer((_, res) => res.end(`My ip address is ${address()}`, 'utf-8'))
+      .createServer((_, res) => res.end(`My ip address is ${ip.address()}`, 'utf-8'))
       .listen(3000, () => console.log('Server is running on the port 3000'))
   );
