671af6f4a41cde12c674720a3dc3a6ba11321226
Иван Подгорнов <34876346+ivan-podgornov@users.noreply.github.com>
2023-09-13 02:48:54 +0300

    ci: setup environment for the cms package (#20)
    Closes #19

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment -- verbatimModuleSyntax and config in ts are incompatible
-// @ts-nocheck
-
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   reporters: ['default', ['github-actions', { silent: false }]],
-  projects: ['api', 'cli', 'core'].map((packageName) => ({
+  projects: ['api', 'cli', 'core', 'cms'].map((packageName) => ({
     displayName: `@colibrijs/${packageName}`,
     testMatch: [`<rootDir>/packages/${packageName}/src/**/*.spec.ts`],
     preset: 'ts-jest/presets/default',
---
+++ packages/cms/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PageIndex() {
+  return <h1>I am an index page</h1>;
+}
