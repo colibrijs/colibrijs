2e7a2af9daf086d336d771fdbdf4e72c4fb47562
Иван Подгорнов <34876346+ivan-podgornov@users.noreply.github.com>
2023-09-13 19:20:59 +0300

    ci: setup environment and deployments for the app (#22)
    Closes #21

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   reporters: ['default', ['github-actions', { silent: false }]],
-  projects: ['api', 'cli', 'core', 'cms'].map((packageName) => ({
+  projects: ['api', 'app', 'cli', 'core', 'cms'].map((packageName) => ({
     displayName: `@colibrijs/${packageName}`,
     testMatch: [`<rootDir>/packages/${packageName}/src/**/*.spec.ts`],
     preset: 'ts-jest/presets/default',
