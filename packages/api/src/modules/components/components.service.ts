18a436cc6f6f8fe1e4df7732eaceef956ab28e8d
Ivan Podgornov <ivan.podgornov@live.com>
2023-10-23 22:55:25 +0300

    feat(api): components gateway

--- packages/api/src/modules/components/components.service.ts
+++ packages/api/src/modules/components/components.service.ts
@@ -3,13 +3,32 @@ import { InjectRepository } from '@nestjs/typeorm';
 import type { Repository } from 'typeorm';
 
 import { Component } from './component.entity';
+import type { ConsturctorOptions, FindOptions } from './component.entity';
 
 @Injectable()
 export class ComponentsService {
   @InjectRepository(Component)
   private readonly components!: Repository<Component>;
 
-  async findById(id: string): Promise<Component | null> {
-    return this.components.findOneBy({ id });
+  /** Creates component with specified options */
+  create(options: ConsturctorOptions): Promise<Component> {
+    return this.components.save(this.components.create(options));
+  }
+
+  /** Find components by specified options */
+  findBy(options: FindOptions): Promise<Component[]> {
+    return this.components.findBy(options);
+  }
+
+  /** Find one component by its id */
+  findById(id: string): Promise<Component> {
+    return this.components.findOneByOrFail({ id });
+  }
+
+  /** Removes component with specified id */
+  async remove(id: string): Promise<Component> {
+    const component = await this.findById(id);
+    this.components.delete({ id });
+    return component;
   }
 }
