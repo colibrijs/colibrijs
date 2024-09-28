06a51d8883977432c7d4b78449c539f5525cf6f9
Ivan Podgornov <ext.ipodgornov@tinkoff.ru>
2024-09-29 01:54:29 +0300

    refactor(core): удаление компонента content-editor-layout

--- packages/core/components/content-editor/__stories__/integration-element-editor.spec.stories.ts
+++ packages/core/components/content-editor/__stories__/integration-element-editor.spec.stories.ts
@@ -1,8 +1,9 @@
 import { exampleElement } from '@colibrijs/mocks/elements';
-import { expect, fn } from '@storybook/test';
+import { expect } from '@storybook/test';
 
 import ContentEditorStoriesMeta from './content-editor.stories';
 import type { Story, StoryMeta } from './content-editor.stories';
+import { withMockedApi } from '../../../hooks/use-api/mocked';
 import { ContentEditorTO } from '../test-object';
 
 export default {
@@ -16,50 +17,46 @@ export const HiddenByDefault: Story = {
     const contentEditor = new ContentEditorTO(context);
     const editor = await contentEditor.getEditorDrawerElement();
 
-    expect(editor).toBeNull();
+    expect(editor, 'Убеждаюсь, что редактор скрыт по умолчанию').toBeNull();
   },
 };
 
 export const VisibleOnSelect: Story = {
   name: 'При выборе элемента в дереве, открывается редактор этого элемента',
-  args: {
-    content: [exampleElement],
-  },
+  decorators: [
+    withMockedApi((apiClient) => {
+      apiClient.override({
+        elements: {
+          get: () => Promise.resolve([exampleElement]),
+        },
+      });
+    }),
+  ],
   play: async (context) => {
     const contentEditor = new ContentEditorTO(context);
     await contentEditor.selectElement(exampleElement.id);
     const editor = await contentEditor.getEditorDrawerElement();
 
-    expect(editor).toBeVisible();
+    expect(editor, 'Убеждаюсь, что редактор отображается').toBeVisible();
   },
 };
 
 export const Title: Story = {
   name: 'В хедере редактора пропсов выводится название компонента',
-  args: {
-    content: [exampleElement],
-  },
+  decorators: [
+    withMockedApi((apiClient) => {
+      apiClient.override({
+        elements: {
+          get: () => Promise.resolve([exampleElement]),
+        },
+      });
+    }),
+  ],
   play: async (context) => {
     const contentEditor = new ContentEditorTO(context);
     await contentEditor.selectElement(exampleElement.id);
     const editorTitle = await contentEditor.getEditorDrawerTitle();
 
     expect(editorTitle).toHaveTextContent(exampleElement.component.name);
-  },
-};
-
-export const EventChange: Story = {
-  name: 'При редактировании пропсов, вызывается onChange с изменённым контентом',
-  args: {
-    content: [exampleElement],
-    onChange: fn(),
-  },
-};
-
-export const EventRemove: Story = {
-  name: 'При удалении элемента, вызывается onChange с изменённым контентом',
-  args: {
-    content: [exampleElement],
-    onChange: fn(),
   },
 };
--- packages/core/components/content-editor/__stories__/tree-content.spec.stories.ts
+++ packages/core/components/content-editor/__stories__/tree-content.spec.stories.ts
@@ -3,6 +3,7 @@ import { expect } from '@storybook/test';
 
 import ContentEditorStoriesMeta from './content-editor.stories';
 import type { Story, StoryMeta } from './content-editor.stories';
+import { withMockedApi } from '../../../hooks/use-api/mocked';
 import { ContentEditorTO } from '../test-object';
 
 export default {
@@ -11,10 +12,16 @@ export default {
 } satisfies StoryMeta;
 
 export const Title: Story = {
-  name: 'Название листочка в дереве элементов равно названию компонента',
-  args: {
-    content: [exampleElement],
-  },
+  name: 'Название листочка в дереве элементов совпадает с названием компонента',
+  decorators: [
+    withMockedApi((apiClient) => {
+      apiClient.override({
+        elements: {
+          get: () => Promise.resolve([exampleElement]),
+        },
+      });
+    }),
+  ],
   play: async (context) => {
     const contentEditor = new ContentEditorTO(context);
     const title = await contentEditor.getTreeNodeTitle(exampleElement.id);
