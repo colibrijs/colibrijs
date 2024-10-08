ed8e23c5dc13f16906d04b4eb8a47cbc8e22c249
Ivan Podgornov <ext.ipodgornov@tinkoff.ru>
2024-10-09 01:59:55 +0300

    feat: скриншоты сохраняются ну очень быстро

--- packages/core/.storybook/test-runner.ts
+++ packages/core/.storybook/test-runner.ts
@@ -1,9 +1,7 @@
 import { type TestRunnerConfig, type TestHook, getStoryContext } from '@storybook/test-runner';
-import fs from 'node:fs';
-import fsp from 'node:fs/promises';
+import fs from 'node:fs/promises';
 import path from 'node:path';
-import pixelmatch from 'pixelmatch';
-import { PNG } from 'pngjs';
+import resemble, { type ComparisonResult } from 'resemblejs';
 
 const REFERENCE_STORYBOOK_URL = 'https://colibrijs.github.io/colibrijs/main/storybook/';
 
@@ -11,17 +9,13 @@ type StoryContext = Awaited<ReturnType<typeof getStoryContext>>;
 type Page = Parameters<TestHook>[0];
 type Story = Parameters<TestHook>[1];
 
-// 21900ms
-
 // проблемы:
-// 1. Вставили 5сек таймаут, нужно понять как дожидаться загрузки страницы - готово
-// 2. Долго сохраняются картинки. Нужно распараллелить скриншоты и сохранение картинок - готово, 0.5с экономии в кармане
-// 3. Пришлось руками править исходник pixelmatch - отложено до лучших времён
-// 5. Сохраняются только 3 картинки. Нужно сохранять картинки в папке actual, diff, reference.
-//    В качестве имени файла указывать id сториса - готово
-// 6. Папки для картинок надо создавать при запуске теста
-// 7. Страницу с reference-сторибуком нужно открывать заранее перед запуском тестов
-// 8. Не обрабатываются ситуации с отсутствием сториса на гитхуб-пегасе
+// 1. Очищать папку со скриншотами перед прогоном тестов
+// 2. Папки для картинок надо создавать при первом сломанном тесте. Сейчас если два теста сломаются,
+//    получим ошибку, так как скриншотер попытается создать уже существующую папку
+// 3. Страницу с reference-сторибуком нужно открывать заранее перед запуском тестов
+// 4. Не обрабатываются ситуации с отсутствием сториса на гитхуб-пегасе
+// 5. Скриншоты не игнорятся гитом
 function getScreenshoterConfig(): TestRunnerConfig {
   async function postVisit(page: Page, story: Story) {
     const context = await getStoryContext(page, story);
@@ -35,35 +29,25 @@ function getScreenshoterConfig(): TestRunnerConfig {
       page.screenshot(),
     ]);
 
-    const reference = PNG.sync.read(referenceScreenshot);
-    const actual = PNG.sync.read(actualScreenshot);
-    const diff = new PNG({ width: reference.width, height: reference.height });
+    const result = await compareImages(referenceScreenshot, actualScreenshot);
 
-    const result = pixelmatch(
-      reference.data,
-      actual.data,
-      diff.data,
-      reference.width,
-      reference.height
-    );
-
-    if (result) {
+    if (result.rawMisMatchPercentage) {
       const outputDir = path.resolve(__dirname, './screenshots/');
       const actualDir = path.resolve(outputDir, './actual/');
       const referenceDir = path.resolve(outputDir, './reference/');
       const diffDir = path.resolve(outputDir, './diff/');
 
-      await fsp.mkdir(outputDir);
-      await Promise.all([fsp.mkdir(actualDir), fsp.mkdir(referenceDir), fsp.mkdir(diffDir)]);
+      await fs.mkdir(outputDir);
+      await Promise.all([fs.mkdir(actualDir), fs.mkdir(referenceDir), fs.mkdir(diffDir)]);
 
       await Promise.all([
-        saveScreenshot(path.resolve(referenceDir, `./${story.id}.png`), reference),
-        saveScreenshot(path.resolve(actualDir, `./${story.id}.png`), actual),
-        saveScreenshot(path.resolve(diffDir, `./${story.id}.png`), diff),
+        saveScreenshot(path.resolve(referenceDir, `./${story.id}.png`), referenceScreenshot),
+        saveScreenshot(path.resolve(actualDir, `./${story.id}.png`), actualScreenshot),
+        saveScreenshot(path.resolve(diffDir, `./${story.id}.png`), result.getBuffer!(false)),
       ]);
     }
 
-    expect(result).toBe(0);
+    expect(result.rawMisMatchPercentage).toBe(0);
   }
 
   function isScreenshotStory(storyData: StoryContext) {
@@ -82,17 +66,14 @@ function getScreenshoterConfig(): TestRunnerConfig {
     return referencePage.screenshot();
   }
 
-  function saveScreenshot(filename: string, image: PNG): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const output = fs.createWriteStream(filename, 'binary');
-      const png = new PNG({ height: image.height, width: image.width });
-
-      image.data.copy(png.data);
-      png.pack().pipe(output);
-
-      output.on('finish', resolve);
-      output.on('error', reject);
+  function compareImages(reference: Buffer, actual: Buffer): Promise<ComparisonResult> {
+    return new Promise((resolve) => {
+      resemble(reference).compareTo(actual).onComplete(resolve);
     });
+  }
+
+  async function saveScreenshot(filename: string, image: Buffer): Promise<void> {
+    await fs.writeFile(filename, image, 'binary');
   }
 
   return { postVisit };
